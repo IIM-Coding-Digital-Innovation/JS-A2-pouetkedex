09bd96b69d3b6446246028b6a72fbe2a1956bcb5
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2023-01-12 18:39:17 +0100

    :sparkles: generate new Mewtwo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let mewtwoCounter = 1
+
 class Pokemon{
     constructor(name,img,hp,attack,defense,speed){
         this.name = name;
@@ -17,7 +19,7 @@ for(let i = 1; i <= 151; i++){
     }
     generatePokemon(i)
 }
-console.log('ok')
+console.log(G1pokemon)
 
 async function getPokemonFromApi(id) {
     let url = "https://pokeapi.co/api/v2/pokemon/" + id.toString();
@@ -38,6 +40,50 @@ function generatePokemon(id){
     })
 }
 
-function returnPokelist(){
-    console.log(G1pokemon);
+function generateMewtwo(){
+    let mewtwo = new Pokemon('Mewtwo-'+mewtwoCounter,
+    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png',
+    randomBetween(40,60),
+    randomBetween(30,50),
+    randomBetween(20,50),
+    randomBetween(70,100))
+    document.getElementById('generateMewtwoImage').style.backgroundImage = "url("+mewtwo.img+")"
+    document.getElementById('mewtwoName').innerHTML = 'name : '+mewtwo.name
+    document.getElementById('mewtwoHp').innerHTML = 'hp : '+mewtwo.hp
+    document.getElementById('mewtwoAttack').innerHTML = 'attack : '+mewtwo.attack
+    document.getElementById('mewtwoDefense').innerHTML = 'defense : '+mewtwo.defense
+    document.getElementById('mewtwoSpeed').innerHTML = 'speed : '+mewtwo.speed
+    createCombatButton(0)
+    mewtwoCounter ++
 }
+
+function createCombatButton(destroy){
+    let combatMod = document.getElementById('combatMod')
+    let check = document.getElementById('combatButton');
+    if(destroy == 1){
+        check.remove()
+    }else{
+        if(check == null){
+            let combatButton = document.createElement("button");
+            combatButton.id = "combatButton";
+            combatButton.innerText = "Start Fight"
+            combatMod.appendChild(combatButton);
+        }
+    }
+}
+
+function createEvolutionButton(){
+    let combatMod = document.getElementById('combatMod')
+    combatMod
+}
+
+function destroyMewtwo(){
+    delete mewtwo
+    document.getElementById('combatButton').destroy()
+    document.getElementById('evolutionButton').destroy()
+}
+
+function randomBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+  
